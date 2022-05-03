171b4ee5d6d39604c57032e6dfff8c62f98ba42d
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-03 14:47:11 +0200

    dichiarazione variabile / richiesta cognome

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 // chiedi all'utente il suo nome, il suo cognome e il suo colore preferito
 
 // dichiarazioni variabili
-
-const name = prompt('Inserisci nome:');
+// name
+const name = prompt('Inserisci Nome:');
 // debug
 console.log(name);
+
+// suname
+const surname = prompt('Inserisci Cognome:');
+// debug
+console.log(surname);
+
+
+
